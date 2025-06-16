f0a4e11264289558b91470420654c988ddf422d2
Donisio Rash <Donisio Rash@DESKTOP-472SS04>
2025-06-15 19:28:12 -0600

    Easy

---
+++ Exercise1/src/unit1/Easy.java
@@ -0,0 +1,13 @@
+package unit1;
+
+import javax.swing.JOptionPane;
+
+public class Easy {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "The backslash character (\\) is easy to print " +
+                "\nThe double quotes  are also “easy” to print");
+
+	}
+
+}
