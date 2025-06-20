7c8315489417e600cca02643495f6fb69493c80e
Donisio Rash <Donisio Rash@DESKTOP-472SS04>
2025-06-19 23:06:14 -0600

    number 1

--- Exercise2/src/unit2/MutiplyAddPrint.java
+++ Exercise2/src/unit2/MutiplyAddPrint.java
@@ -1,9 +1,31 @@
 package unit2;
 
+import javax.swing.JOptionPane;
+
 public class MutiplyAddPrint {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String input1 = JOptionPane.showInputDialog("Enter first number:");
+        int num1 = Integer.parseInt(input1);
+        
+        String input2 = JOptionPane.showInputDialog("Enter second number:");
+        int num2 = Integer.parseInt(input2);
+        
+        int result1 = num1 + num2;
+        double result2 = num1 * num2;
+        
+        String comparisonMessage;
+        if (num1 > num2) {
+            comparisonMessage = "The first number (" + num1 + ") is larger.";
+        } else if (num2 > num1) {
+            comparisonMessage = "The second number (" + num2 + ") is larger.";
+        } else {
+            comparisonMessage = "Both numbers are equal.";
+        }
+        
+
+        JOptionPane.showMessageDialog(null, "•	the value of them added together : " + result1 + "\n•	the value of them multiplied together : " + result2 +
+        		"\n• " + comparisonMessage);
 
 	}
 
