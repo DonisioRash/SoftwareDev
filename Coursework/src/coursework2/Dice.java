147e98db83e8a2dce039a27ba1b17e3d8cd27fbb
Donisio Rash <Donisio Rash@DESKTOP-472SS04>
2025-08-08 20:03:35 -0600

    add

--- Coursework/src/coursework2/Dice.java
+++ Coursework/src/coursework2/Dice.java
@@ -1,16 +1,23 @@
 package coursework2;
 
-import java.util.Random;
+import java.util.Random; // Import Random class to generate random numbers
 
-public class Dice {
-	private static final Random rand = new Random();
-	private int value;
+public class Dice { // Represents a single six-sided dice
 
+	// Class variables
+	private static final Random rand = new Random(); // One shared Random object for all Dice objects
+	private int value; // Stores the current value of the dice roll (1–6)
+
+	// Constructor
 	public Dice() {
+		// When a Dice object is created, it "rolls" immediately
+		// rand.nextInt(6) generates 0–5, adding 1 makes it 1–6
 		value = rand.nextInt(6) + 1;
 	}
 
+	// Getter method
 	public int getValue() {
+		// Returns the current value of the dice
 		return value;
 	}
 }
--- Coursework/src/coursework2/DicePoker.java
+++ Coursework/src/coursework2/DicePoker.java
@@ -1,16 +1,27 @@
 package coursework2;
 
-public class DicePoker {
+public class DicePoker { // Main entry point for the Dice Poker game
+
 	public static void main(String[] args) {
+		// Main game loop for handling multiple players
 		do {
+			// Create a new Game object and start playing for the current player
 			new Game().play();
-		} while (askToAddAnotherPlayer());
+		}
+		// After the game ends, ask if another player wants to play
+		while (askToAddAnotherPlayer());
+
+		// Show a final thank-you message after all players have finished
 		UI.showMessage(" Thank you for playing Dice Poker!");
 	}
 
+	// ==== Method to ask if a new player should be added ====
 	public static boolean askToAddAnotherPlayer() {
+		// Show a Yes/No confirmation dialog to the user
 		int choice = javax.swing.JOptionPane.showConfirmDialog(null, "Add another player?", "Add Player",
 				javax.swing.JOptionPane.YES_NO_OPTION);
+
+		// Return true if player clicked "Yes", otherwise false
 		return choice == javax.swing.JOptionPane.YES_OPTION;
 	}
 }
